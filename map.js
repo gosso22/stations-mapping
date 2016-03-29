2820f37006215b00f0255147b67c33cd3ecf405b
Kassim Sheghembe <sheghembekassim@gmail.com>
2016-03-29 11:30:50 +0300

    add map component boilerplate

---
+++ map.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div />
+    )
+  }
+}
+
+export default Map
