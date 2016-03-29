95142b3e27aa0df33e7dc1cb31e77e0349e54ebf
Kassim Sheghembe <sheghembekassim@gmail.com>
2016-03-29 11:38:40 +0300

    create test map using google map component

--- map.js
+++ map.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
+import { GoogleMap } 
+  from 'react-google-maps'
+
 class Map extends React.Component {
   constructor(props) {
     super(props)
   }
   render() {
     return (
-      <div />
+      <GoogleMap
+        containerProps={{
+          ...this.props, 
+          style: {
+            height: '100%',
+          },
+        }}
+        defaultZoom={8}
+        defaultCenter={{ 
+          lat: -34.397, 
+          lng: 150.644 
+        }}
+      />
     )
   }
 }
