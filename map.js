b8d823c8d209554ea22c598bb01fd379c5f51482
Kassim Sheghembe <sheghembekassim@gmail.com>
2016-03-30 10:42:27 +0300

    add map from leaflet examples

--- map.js
+++ map.js
@@ -4,13 +4,19 @@ import { Map, Marker, Popup, TileLayer }
   from 'react-leaflet'
 
 class LeafletMap extends React.Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     const position = [51.505, -0.09]
     return (
-      <Map center={position} zoom={13}>
+      <Map center={position} zoom={13} style={{height: '600px'}}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+          </Popup>
+        </Marker>
       </Map>
     )
   }
