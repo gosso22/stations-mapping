2b3f6cce48606041eed92b7d84647d6542bec97e
Kassim Sheghembe <sheghembekassim@gmail.com>
2016-03-31 10:16:22 +0300

    add mockup stations data

--- main.js
+++ main.js
@@ -2,6 +2,81 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
+const stationsData = {
+  stations: [
+    {
+      name: 'Radio 5',
+      location: {
+        longitude: 0,
+        latitude: 0,
+      },
+      frequency: '',
+      contacts: {},
+      populationCoverage: 100,
+    },
+    {
+      name: 'Radio React',
+      location: {
+        longitude: 0,
+        latitude: 0,
+      },
+      frequency: '',
+      contacts: {},
+      populationCoverage: 100,
+    },
+    {
+      name: 'Radio Farm Radio',
+      location: {
+        longitude: 0,
+        latitude: 0,
+      },
+      frequency: '',
+      contacts: {},
+      populationCoverage: 100,
+    },
+    {
+      name: 'Faraja FM',
+      location: {
+        longitude: 0,
+        latitude: 0,
+      },
+      frequency: '',
+      contacts: {},
+      populationCoverage: 100,
+    },
+    {
+      name: 'Triple-A',
+      location: {
+        longitude: 0,
+        latitude: 0,
+      },
+      frequency: '',
+      contacts: {},
+      populationCoverage: 100,
+    },
+    {
+      name: 'Radio Mario',
+      location: {
+        longitude: 0,
+        latitude: 0,
+      },
+      frequency: '',
+      contacts: {},
+      populationCoverage: 100,
+    },
+    {
+      name: 'Radio Maria',
+      location: {
+        longitude: 0,
+        latitude: 0,
+      },
+      frequency: '',
+      contacts: {},
+      populationCoverage: 100,
+    },
+  ],
+}
+
 ReactDOM.render(
   <App />,
   document.getElementById('main')
