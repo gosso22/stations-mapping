d0c56c1f52abe4e1e7e885d0b50dccda1559842e
Kassim Sheghembe <sheghembekassim@gmail.com>
2016-04-01 11:13:28 +0300

    change map params

--- map.js
+++ map.js
@@ -7,7 +7,7 @@ class LeafletMap extends React.Component {
   render() {
     const position = [-6.00, 35.00]
     return (
-      <Map center={position} zoom={13} style={{height: '600px'}}>
+      <Map center={position} zoom={6} style={{height: '600px'}}>
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
